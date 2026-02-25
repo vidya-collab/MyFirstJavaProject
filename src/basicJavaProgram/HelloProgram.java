068c806755141eb40241836d6b12622fe8dfa93e
Vidya <vidya.kushte@gmail.com>
2026-02-25 09:53:05 -0600

    Java Program

---
+++ src/basicJavaProgram/HelloProgram.java
@@ -0,0 +1,27 @@
+package basicJavaProgram;
+
+
+import java.util.Scanner;
+
+public class HelloProgram
+{
+public static void main(String [] args)
+
+{
+    System.out.println("Hello world");
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter your name: ");
+    String name = scanner.nextLine();
+    System.out.println("Hello " + name + "!");
+
+    System.out.println("Enter your age");
+    int age= scanner.nextInt();
+    System.out.println("Hello " + name + ", you are " + age + " years old.");
+
+    scanner.close();
+
+
+
+}
+}
