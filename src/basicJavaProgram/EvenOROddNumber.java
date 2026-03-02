ef15fafa65f1a5c068b190a5f74fdf580d831e2d
Vidya <vidya.kushte@gmail.com>
2026-03-02 14:49:48 -0600

    Java Program

---
+++ src/basicJavaProgram/EvenOROddNumber.java
@@ -0,0 +1,14 @@
+package basicJavaProgram;
+
+public class EvenOROddNumber {
+
+    public static void main(String[] args) {
+        int number = 10; // You can change this number to test with different values
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+    }
+}
