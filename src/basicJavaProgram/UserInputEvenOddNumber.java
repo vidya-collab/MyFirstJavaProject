c40747d2a6e020bba38eae21d83bb35691ee7c66
Vidya <vidya.kushte@gmail.com>
2026-03-02 14:54:11 -0600

    Java Program

---
+++ src/basicJavaProgram/UserInputEvenOddNumber.java
@@ -0,0 +1,22 @@
+package basicJavaProgram;
+
+import java.util.Scanner;
+
+public class UserInputEvenOddNumber {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        int number = sc.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even");
+        } else {
+            System.out.println(number + " is Odd");
+        }
+
+        sc.close();
+    }
+}
